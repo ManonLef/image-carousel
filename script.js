118355cd10de2f246c120215f8ff65832d2da698
Manon <manonlefkos@gmail.com>
2023-04-22 19:49:55 +0200

    add circle selectors function

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const images = document.querySelectorAll("img")
-const next = document.querySelector(".right-arrow")
-const previous = document.querySelector(".left-arrow")
+// consider wrapping in IIFE
+const images = document.querySelectorAll("img");
+const next = document.querySelector(".right-arrow");
+const previous = document.querySelector(".left-arrow");
+let currentIndex = 0;
 
 function hide(index) {
-  images[index].setAttribute("hidden", true)
+  images[index].setAttribute("hidden", true);
 }
 
 function unhide(index) {
-  images[index].removeAttribute("hidden")
+  images[index].removeAttribute("hidden");
 }
+
+(function addCircleSelectors() {
+  const circleContainer = document.querySelector(".circles");
+  for (let i = 0; i < images.length; i++) {
+    const newCircle = document.createElement("div");
+    newCircle.setAttribute("data-index", i);
+    newCircle.className = "circle";
+    newCircle.textContent = "⚪";
+    circleContainer.appendChild(newCircle);
+    // add eventlistener
+  }
+})()
